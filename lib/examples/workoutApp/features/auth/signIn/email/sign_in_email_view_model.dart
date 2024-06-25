4866e80d854e4b317348e7a398593842759c5da0
Kwe <Kweayon@gmail.com>
2024-06-25 15:34:11 -0500

    feat: enail signin view

---
+++ lib/examples/workoutApp/features/auth/signIn/email/sign_in_email_view_model.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+// TODO: use firebase for auth
+
+class SignInEmailViewModel extends ChangeNotifier {
+  String? _email;
+
+  String? get email => _email;
+
+  void setEmail(String email) {
+    _email = email;
+    debugPrint("from $this.setEmail: $email");
+    notifyListeners();
+  }
+}
