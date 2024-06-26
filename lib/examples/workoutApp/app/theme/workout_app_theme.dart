eb6d17d0bbe13f79edcbfadbeb5b16c0fbf79017
Kwe <Kweayon@gmail.com>
2024-06-26 11:36:46 -0500

    feat: broke ThemeData properties passed in into individual variables

--- lib/examples/workoutApp/app/theme/workout_app_theme.dart
+++ lib/examples/workoutApp/app/theme/workout_app_theme.dart
@@ -1,3 +1,4 @@
+import 'package:example_ui/examples/workoutApp/app/theme/colors.dart';
 import 'package:flutter/material.dart';
 
 class WorkoutAppTheme {
@@ -16,53 +17,83 @@ class WorkoutAppTheme {
       //   onSurface: Colors.lightGreen,
       // ),
       colorScheme: const ColorScheme.dark(
-        primary: Color(0xff7C4DFF),
+        primary: WorkoutAppColors.mainThemeColor,
       ),
       // scaffoldBackgroundColor: Colors.black,
-      inputDecorationTheme: const InputDecorationTheme(
-        filled: true,
-        fillColor: Color(0xff202023),
-        border: OutlineInputBorder(
-          borderSide: BorderSide.none,
-          borderRadius: BorderRadius.all(
-            Radius.circular(16),
-          ),
-        ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(
-            width: 2,
-            color: Color(0xff7C4DFF),
-          ),
-          borderRadius: BorderRadius.all(
-            Radius.circular(16),
-          ),
-        ),
-      ),
-
-      outlinedButtonTheme: OutlinedButtonThemeData(
-        style: ButtonStyle(
-          side: resolver(BorderSide.none),
-          shape: resolver(
-            const RoundedRectangleBorder(
-              borderRadius: BorderRadius.all(
-                Radius.circular(16),
-              ),
-            ),
-          ),
-          backgroundColor: resolver(
-            const Color(0xff7C4DFF),
-          ),
-          textStyle: resolver(
-            TextStyle(
-              foreground: Paint()..color = Colors.white,
-              fontSize: 16,
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-        ),
-      ),
+      inputDecorationTheme: inputDecorationTheme,
+      outlinedButtonTheme: mainButtonTheme,
+      bottomNavigationBarTheme: bottomNavigationBarTheme,
     );
   }
 }
 
+const inputDecorationTheme = InputDecorationTheme(
+  filled: true,
+  fillColor: Color(0xff202023),
+  border: OutlineInputBorder(
+    borderSide: BorderSide.none,
+    borderRadius: BorderRadius.all(
+      Radius.circular(16),
+    ),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(
+      width: 2,
+      color: WorkoutAppColors.mainThemeColor,
+    ),
+    borderRadius: BorderRadius.all(
+      Radius.circular(16),
+    ),
+  ),
+);
+
+final mainButtonTheme = OutlinedButtonThemeData(style: mainButtonStyle);
+final seondaryButtonTheme = OutlinedButtonThemeData(style: secondaryButtonStyle);
+
+final mainButtonStyle = ButtonStyle(
+  side: resolver(BorderSide.none),
+  shape: resolver(
+    const RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(
+        Radius.circular(16),
+      ),
+    ),
+  ),
+  backgroundColor: resolver(
+    WorkoutAppColors.mainThemeColor,
+  ),
+  textStyle: resolver(
+    TextStyle(
+      foreground: Paint()..color = Colors.white,
+      fontSize: 16,
+      fontWeight: FontWeight.w700,
+    ),
+  ),
+);
+final secondaryButtonStyle = mainButtonStyle.copyWith(
+  backgroundColor: resolver(
+    Colors.white,
+  ),
+  textStyle: resolver(
+    TextStyle(
+      foreground: Paint()..color = WorkoutAppColors.mainThemeColor,
+      fontSize: 14,
+      fontWeight: FontWeight.w700,
+    ),
+  ),
+);
+
+const bottomNavigationBarTheme = BottomNavigationBarThemeData(
+  backgroundColor: WorkoutAppColors.mainThemeColor,
+  type: BottomNavigationBarType.fixed,
+  showUnselectedLabels: true,
+  selectedItemColor: Colors.white,
+  unselectedItemColor: Colors.white,
+);
+
 WidgetStateProperty<T> resolver<T>(T obj) => WidgetStateProperty.resolveWith((_) => obj);
+
+
+// BottomNavigationBarType.fixed
+
+//   - fixes BottomNavigationBarItem in places and removes dynamic padding when selecting items
