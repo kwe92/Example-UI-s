47bf5b003a3c974adaa309195ac68775efba32bb
Kwe <Kweayon@gmail.com>
2024-06-26 11:37:34 -0500

    feat: initial NavigatorView with bottom nav bar

---
+++ lib/examples/workoutApp/features/navigator/navigator_view.dart
@@ -0,0 +1,66 @@
+import 'package:example_ui/examples/workoutApp/features/navigator/navigator_view_model.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:provider/provider.dart';
+
+class NavigatorView extends StatelessWidget {
+  const NavigatorView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<NavigatorViewModel>(
+      create: (_) => NavigatorViewModel(),
+      builder: (context, _) {
+        final viewModel = context.watch<NavigatorViewModel>();
+
+        return SafeArea(
+          child: Scaffold(
+            body: viewModel.currentChild[viewModel.selectedIndex],
+            bottomNavigationBar: BottomNavigationBar(
+              currentIndex: viewModel.selectedIndex,
+              onTap: viewModel.setSelectedIndex,
+              items: [
+                BottomNavigationBarItem(
+                  icon: Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: SvgPicture.asset(viewModel.selectedIndex == 0
+                        ? "/Users/kwe/flutter-projects/Example-UI-FLutter/example_ui/lib/examples/workoutApp/app/assets/home_icon_active.svg"
+                        : "/Users/kwe/flutter-projects/Example-UI-FLutter/example_ui/lib/examples/workoutApp/app/assets/home_icon.svg"),
+                  ),
+                  label: "Home",
+                ),
+                BottomNavigationBarItem(
+                  icon: Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: SvgPicture.asset(viewModel.selectedIndex == 1
+                        ? "/Users/kwe/flutter-projects/Example-UI-FLutter/example_ui/lib/examples/workoutApp/app/assets/activity_icon_active.svg"
+                        : "/Users/kwe/flutter-projects/Example-UI-FLutter/example_ui/lib/examples/workoutApp/app/assets/activity_icon.svg"),
+                  ),
+                  label: "Activity",
+                ),
+                BottomNavigationBarItem(
+                  icon: Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: SvgPicture.asset(viewModel.selectedIndex == 2
+                        ? "/Users/kwe/flutter-projects/Example-UI-FLutter/example_ui/lib/examples/workoutApp/app/assets/calendar_icon_active.svg"
+                        : "/Users/kwe/flutter-projects/Example-UI-FLutter/example_ui/lib/examples/workoutApp/app/assets/calendar_icon.svg"),
+                  ),
+                  label: "Calendar",
+                ),
+                BottomNavigationBarItem(
+                  icon: Padding(
+                    padding: const EdgeInsets.only(top: 8.0),
+                    child: SvgPicture.asset(viewModel.selectedIndex == 3
+                        ? "/Users/kwe/flutter-projects/Example-UI-FLutter/example_ui/lib/examples/workoutApp/app/assets/profile_icon_active.svg"
+                        : "/Users/kwe/flutter-projects/Example-UI-FLutter/example_ui/lib/examples/workoutApp/app/assets/profile_icon.svg"),
+                  ),
+                  label: "Profile",
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/examples/workoutApp/features/navigator/navigator_view_model.dart
@@ -0,0 +1,22 @@
+import 'package:example_ui/examples/workoutApp/features/dashboard/ui/dashboard_view.dart';
+import 'package:flutter/material.dart';
+
+class NavigatorViewModel extends ChangeNotifier {
+  int _selectedIndex = 0;
+
+  int get selectedIndex => _selectedIndex;
+
+  final List<Widget> _currentChild = [
+    const DashboardView(),
+    const DashboardView(),
+    const DashboardView(),
+    const DashboardView(),
+  ];
+
+  List<Widget> get currentChild => _currentChild;
+
+  void setSelectedIndex(int index) {
+    _selectedIndex = index;
+    notifyListeners();
+  }
+}
