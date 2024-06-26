18902b408d89ee668821e618eecf8563d3df88d2
Kwe <Kweayon@gmail.com>
2024-06-26 11:37:42 -0500

    feat: misc

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:example_ui/examples/workoutApp/app/theme/workout_app_theme.dart';
 import 'package:example_ui/examples/workoutApp/features/auth/signIn/email/sign_in_email_view.dart';
+import 'package:example_ui/examples/workoutApp/features/dashboard/ui/dashboard_view.dart';
+import 'package:example_ui/examples/workoutApp/features/navigator/navigator_view.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     MaterialApp(
       theme: WorkoutAppTheme.getTheme(),
-      home: SignInEmailView(),
+      home: const NavigatorView(),
     ),
   );
 }
