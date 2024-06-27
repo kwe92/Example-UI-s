6d7c17d3410a77392d9b8002d92b7b2c09fd649f
Kwe <Kweayon@gmail.com>
2024-06-26 19:52:06 -0500

    feat: travel app from my FlutterWidget repo

---
+++ lib/examples/travelApp/features/home/destination_selection_view_model.dart
@@ -0,0 +1,27 @@
+import 'package:example_ui/examples/travelApp/features/shared/model/destination.dart';
+import 'package:example_ui/examples/travelApp/features/shared/services/destination_service.dart';
+import 'package:flutter/material.dart';
+
+class DestinationSelectionViewModel extends ChangeNotifier {
+  int _selectedIndex = 0;
+
+  int get selectedIndex => _selectedIndex;
+
+  bool _showBottomNav = false;
+
+  bool get showBottomNav => _showBottomNav;
+
+  List<Destination> get destinations => destinationService.destinations;
+
+  void setIndex(int index) {
+    _selectedIndex = index;
+    notifyListeners();
+  }
+
+  void setShowBottomNav(bool showNav) {
+    _showBottomNav = showNav;
+    notifyListeners();
+  }
+
+  Future<List<Destination>> getData() async => await destinationService.fetchData();
+}
