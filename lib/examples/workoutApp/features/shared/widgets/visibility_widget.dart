4357fdd6b8d1c3baab94efd117a0a187c3be0cdb
Kwe <Kweayon@gmail.com>
2024-06-28 12:12:39 -0500

    feat: moved shared wiget to VisibilityWidget

---
+++ lib/examples/workoutApp/features/shared/widgets/visibility_widget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class VisibilityWidget extends StatelessWidget {
+  final VoidCallback onTap;
+  final bool isObscured;
+  final EdgeInsetsGeometry padding;
+  final double size;
+  final Color color;
+
+  const VisibilityWidget({
+    required this.onTap,
+    required this.isObscured,
+    this.padding = const EdgeInsets.only(right: 16.0),
+    this.size = 24,
+    this.color = const Color(0xff81809E),
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) => Padding(
+        padding: padding,
+        child: IconButton(
+          onPressed: onTap,
+          icon: Icon(
+            size: size,
+            !isObscured ? Icons.visibility_outlined : Icons.visibility_off_outlined,
+          ),
+          color: color,
+        ),
+      );
+}
