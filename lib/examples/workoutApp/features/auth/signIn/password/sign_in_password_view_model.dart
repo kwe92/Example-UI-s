248c2a9607a5360c8e87b2dc67484acbf99866d0
Kwe <Kweayon@gmail.com>
2024-07-01 10:37:19 -0500

    feat: added error handling to signInWithEmailAndPassword implementation | added successfulLogin derived method

--- lib/examples/workoutApp/features/auth/signIn/password/sign_in_password_view_model.dart
+++ lib/examples/workoutApp/features/auth/signIn/password/sign_in_password_view_model.dart
@@ -1,4 +1,5 @@
 import 'package:example_ui/examples/workoutApp/features/auth/services/auth_service.dart';
+import 'package:example_ui/examples/workoutApp/features/services/toast_service.dart';
 import 'package:example_ui/examples/workoutApp/features/shared/mixins/extended_change_notifier.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +13,8 @@ class SignInPasswordViewModel extends ExtendedChangeNotifier {
   String? get password => _password;
 
   bool get isObscured => _isObscured;
+
+  bool get successfulLogin => _authServer.loggedIn;
 
   void setPassword(String password) {
     _password = password.trim();
@@ -29,8 +32,25 @@ class SignInPasswordViewModel extends ExtendedChangeNotifier {
   }
 
   Future<void> signInWithEmailAndPassword() async {
-    setBusy(true);
-    await _authServer.signInWithEmailAndPassword();
-    setBusy(false);
+    try {
+      setBusy(true);
+      await _authServer.signInWithEmailAndPassword();
+      setBusy(false);
+      _authServer.setLoggedIn(true);
+    } catch (err) {
+      setBusy(false);
+
+      debugPrint("Error - signInWithEmailAndPassword: ${err.toString()}");
+
+      final errStr = err.toString().toLowerCase();
+
+      if (errStr.contains("invalid-credential")) {
+        toastService.showSnackBar("incorrect email or password.", Colors.red);
+      } else if (errStr.contains("network error")) {
+        toastService.showSnackBar("please check your network connection.", Colors.red);
+      } else {
+        toastService.showSnackBar("an error has occured please try again.", Colors.red);
+      }
+    }
   }
 }
