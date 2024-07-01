e547ea3c5e5cb8269f156e5c0e81da180be32b3f
Kwe <Kweayon@gmail.com>
2024-07-01 10:33:25 -0500

    feat: added a logged in feature

--- lib/examples/workoutApp/features/auth/services/auth_service.dart
+++ lib/examples/workoutApp/features/auth/services/auth_service.dart
@@ -5,7 +5,6 @@ import 'package:example_ui/examples/workoutApp/features/shared/models/user_model
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-// TODO: review thought process
 class AuthService extends ChangeNotifier {
   UserModel _tempUser = UserModel(id: "", fullName: "", email: "", password: "");
 
@@ -15,13 +14,17 @@ class AuthService extends ChangeNotifier {
 
   User? get firebaseUser => _firebaseUser;
 
+  bool _loggedIn = false;
+
+  bool get loggedIn => _loggedIn;
+
   static final _firebaseAuth = FirebaseService.instance().authInstance;
 
   static final AuthService _singleton = AuthService._internal();
 
-  AuthService._internal();
-
   factory AuthService.instance() => _singleton;
+
+  AuthService._internal();
 
   void setTempUserEmail(String email) {
     _tempUser.email = email;
@@ -41,16 +44,17 @@ class AuthService extends ChangeNotifier {
     notifyListeners();
   }
 
+  void setLoggedIn(bool isLoggedIn) {
+    _loggedIn = isLoggedIn;
+    notifyListeners();
+  }
+
   Future<void> createUserWithEmailAndPassword() async {
-    // TODO: add try catch?
+    debugPrint("createUserWithEmailAndPassword:  _tempUser: $_tempUser");
     UserCredential firebaseUserCredentials =
         await _firebaseAuth.createUserWithEmailAndPassword(email: _tempUser.email!, password: _tempUser.password!);
 
     _firebaseUser = firebaseUserCredentials.user;
-
-    // TODO: set UID in firebase | atm the uid is the users email which may not be unique
-
-    _tempUser.id = _firebaseUser?.uid ?? "";
 
     debugPrint("New user created successfully: $_firebaseUser");
 
@@ -58,14 +62,21 @@ class AuthService extends ChangeNotifier {
   }
 
   Future<void> signInWithEmailAndPassword() async {
-    // TODO: add try catch?
-
     var userCredential = await _firebaseAuth.signInWithEmailAndPassword(email: _tempUser.email!, password: _tempUser.password!);
 
     _firebaseUser = userCredential.user;
 
     debugPrint("current user: $_firebaseUser");
 
+    notifyListeners();
+  }
+
+  Future<void> deleteUser() async {
+    await _firebaseUser?.delete();
+  }
+
+  void resetResources() {
+    _tempUser = UserModel(id: "", fullName: "", email: "", password: "");
     notifyListeners();
   }
 }
