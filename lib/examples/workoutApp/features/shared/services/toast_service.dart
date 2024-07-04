542490cec73b07d4233a761fe8259616623606dc
Kwe <Kweayon@gmail.com>
2024-07-04 14:37:29 -0500

    feat: implemented forgotPasswordModal method

--- lib/examples/workoutApp/features/shared/services/toast_service.dart
+++ lib/examples/workoutApp/features/shared/services/toast_service.dart
@@ -1,10 +1,14 @@
 import 'package:example_ui/examples/workoutApp/app/constants/constants.dart';
+import 'package:example_ui/examples/workoutApp/app/theme/workout_app_theme.dart';
 import 'package:example_ui/examples/workoutApp/features/dashboard/model/notification.dart';
 import 'package:example_ui/examples/workoutApp/features/dashboard/model/workout_progress.dart';
+import 'package:example_ui/examples/workoutApp/features/shared/services/services.dart';
 import 'package:example_ui/examples/workoutApp/features/shared/widgets/custom_button.dart';
 import 'package:example_ui/examples/workoutApp/features/shared/widgets/progress_counter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+
+// TODO: refactor as this module is quite big
 
 class ToastService {
   static final ToastService _singleton = ToastService._internal();
@@ -12,6 +16,7 @@ class ToastService {
   factory ToastService() => _singleton;
 
   ToastService._internal();
+
   Future<T?> notificationsModal<T>({required List<AppNotification> notifications, Color barrierColor = Colors.transparent}) {
     return showDialog<T>(
       barrierColor: barrierColor,
@@ -185,6 +190,60 @@ class ToastService {
           );
         },
       );
+
+  Future<bool> forgotPasswordModal() async {
+    return await showDialog<bool>(
+          context: WidgetKey.navigatorKey.currentState!.context,
+          builder: (context) {
+            return Dialog(
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 24),
+                width: double.maxFinite,
+                height: 250,
+                decoration: const BoxDecoration(
+                  color: Color(0xff202023),
+                  borderRadius: BorderRadius.all(Radius.circular(16)),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Are you sre you want to reset your password?",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+                    ),
+                    Text(
+                      "an email will be sent to: " "${authService.tempUser.email}",
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                    ),
+                    Row(
+                      children: [
+                        Flexible(
+                          child: CustomButton(
+                            buttonTheme: secondaryButtonTheme,
+                            onTap: () => Navigator.pop(context, true),
+                            label: 'Yes',
+                          ),
+                        ),
+                        const SizedBox(width: 12),
+                        Flexible(
+                          child: CustomButton(
+                            onTap: () => Navigator.pop(context, false),
+                            label: 'No',
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
+        ) ??
+        false;
+  }
 }
 
 class NotificationListTile extends StatelessWidget {
