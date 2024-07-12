ef70352b749ba79fc6f49f4a2137fd7c3ea4ff7e
Kwe <Kweayon@gmail.com>
2024-07-12 11:58:24 -0500

    feat: added comments

--- test_driver/integration_test.dart
+++ test_driver/integration_test.dart
@@ -1,10 +1,15 @@
 import 'package:integration_test/integration_test_driver.dart';
 
+// TODO: add bash script to run test for you
+
 /// https://github.com/flutter/flutter/tree/main/packages/integration_test#using-flutter-driver-to-run-tests
 
-/*flutter drive \
+/*
+
+flutter drive \
     --driver=test_driver/integration_test.dart \
-    --target=integration_test/foo_test.dart
+    --target=integration_test/sign_in_test.dart
+    
 */
 
 Future<void> main() => integrationDriver();
