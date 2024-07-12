d0148fc0ad769d942fc877edc76d1b5ad9908c92
Kwe <Kweayon@gmail.com>
2024-07-12 11:56:53 -0500

    refact: used robot to abstract away test implementation details

--- test/features/auth/signIn/email/sign_in_email_view_test.dart
+++ test/features/auth/signIn/email/sign_in_email_view_test.dart
@@ -1,97 +1,24 @@
-import 'package:example_ui/features/auth/signIn/email/sign_in_email_view.dart';
-import 'package:example_ui/features/auth/signIn/password/sign_in_password_view_model.dart';
-import 'package:example_ui/features/shared/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:provider/provider.dart';
-
+import '../../../../robots/auth_robot.dart';
 import '../../../../setup/test_helper_mocks.dart';
 
 void main() {
   group(
-    "Intengreation Test - Signin",
+    "SignInEmailView - ",
     () {
       testWidgets(
-        "when a registered user signs in successfully then they are taken to the dashboard view",
+        "when signin view loaded then signin widgets are visible",
         (tester) async {
           FlutterError.onError = ignoreOverflowErrors;
 
-          // pumpSignInScreen
+          final robot = AuthRobot(tester);
 
-          final signInTextFinder = find.text("Sign In");
+          await robot.pumpSignInScreen();
 
-          // debugPrint("$textFinder");
-
-          final emailTextFieldFinder = find.byType(TextFormField);
-
-          // debugPrint("$emailTextFieldFinder");
-
-          final continueButtonFinder = find.byType(CustomButton);
-
-          // debugPrint("$continueButtonFinder");
-
-          expect(signInTextFinder, findsOneWidget);
-
-          expect(emailTextFieldFinder, findsOneWidget);
-
-          expect(continueButtonFinder, findsOneWidget);
-
-          await tester.enterText(emailTextFieldFinder, "test1101@gmail.com");
-
-          await tester.tap(continueButtonFinder);
-
-          await tester.pumpAndSettle(const Duration(seconds: 1));
-
-          // debugPrint("${find.byType(Text)}");
-
-          final passwordTextFinder = find.text("Password");
-
-          expect(passwordTextFinder, findsOneWidget);
-
-          final passwordTextFormField = find.byType(TextFormField, skipOffstage: false).last;
-
-          // debugPrint("${find.byType(TextFormField)}");
-
-          expect(passwordTextFormField, findsOneWidget);
-
-          // await tester.drag(passwordTextFormField, Offset(-500, 0));
-
-          await tester.enterText(passwordTextFormField, "Leibei11!!");
-
-          final signInButtonFinder = find.byType(CustomButton);
-
-          expect(signInButtonFinder, findsOneWidget);
-
-          await tester.tap(signInButtonFinder);
-
-          await tester.pumpAndSettle(const Duration(seconds: 1));
-
-          debugPrint("${find.byType(Text)}");
+          await robot.verifySignInView();
         },
       );
     },
   );
-}
-
-class AuthRobot {
-  final WidgetTester tester;
-
-  AuthRobot(this.tester);
-
-  Future<void> pumpSignInScreen() async {
-    final mockAuthService = TestHelperMocks.getMockAuthService();
-
-    await tester.pumpWidget(
-      MultiProvider(
-        providers: [
-          ChangeNotifierProvider(
-            create: (context) => SignInPasswordViewModel(mockAuthService),
-          ),
-        ],
-        child: MaterialApp(
-          home: SignInEmailView(),
-        ),
-      ),
-    );
-  }
 }
