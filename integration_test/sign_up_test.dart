503056174ca76560740c4cc7d95c307becb694d2
Kwe <Kweayon@gmail.com>
2024-07-13 00:19:44 -0500

    feat: initial SignupView integration tests

---
+++ integration_test/sign_up_test.dart
@@ -0,0 +1,38 @@
+import 'package:example_ui/features/auth/signUp/sign_up_view.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import '../test/robots/auth_robot.dart';
+import '../test/setup/test_helper_mocks.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group("SignupView - Test", () {
+    testWidgets("when a user signs up successfully then they are taken to the dashboard", (tester) async {
+      final authRobot = AuthRobot(tester);
+
+      await TestHelperMocks.pumpViewWithMocks(SignUpView(), tester);
+
+      await authRobot.enterNewUserNameAndEmail();
+
+      await testDelay();
+
+      await authRobot.tapContinueButton();
+
+      await testDelay();
+
+      await authRobot.enterNewUserPassword();
+
+      await testDelay();
+
+      await authRobot.tapContinueButton();
+
+      await testDelay();
+
+      authRobot.expectDashboardText();
+
+      await testDelay();
+    });
+  });
+}
