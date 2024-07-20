8b6406c7c2ecc878bac54528468abb422acb6e41
Kwe <Kweayon@gmail.com>
2024-07-20 17:57:29 -0500

    feat: added dashboard tests

---
+++ test/features/dashboard/ui/dashboard_view_model_test.dart
@@ -0,0 +1,23 @@
+import 'package:example_ui/features/dashboard/ui/dashboard_view_model.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import '../../../setup/test_data.dart';
+import '../../../setup/test_helper_mocks.dart';
+
+void main() {
+  test('when getNotifications called then notifications are fetched', () async {
+    //  Arrange
+    final mockNotificationService = TestHelperMocks.getMockNotificationService();
+
+    final mockToastService = TestHelperMocks.getMockToastService();
+
+    final viewModel = DashboardViewModel(mockNotificationService, mockToastService);
+
+    // Act
+    await viewModel.getNotifications();
+
+    // Assert
+
+    expect(viewModel.notifications, testNotifications);
+  });
+}
